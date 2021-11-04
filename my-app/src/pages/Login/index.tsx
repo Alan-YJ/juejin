ebef036210fb0372069f96db549f71dfd1272104
yijing <734732794@qq.com>
2021-11-04 17:43:56 +0800

    提交

---
+++ my-app/src/pages/Login/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import {Form, Input, Field, Icon, Grid} from '@alifd/next';
+import styles from './index.module.scss'
+
+const {Row, Col} = Grid
+
+const Login = () => {
+  return <div className={styles.loginPage}>
+    <div className="login-wrap">
+      <div>
+        <Row>
+          <Col span="12">img</Col>
+          <Col span="12">
+            <h1>登录</h1>
+            <div></div>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  </div>;
+};
+
+export default Login;
---
+++ my-app/src/routes.ts
@@ -0,0 +1,10 @@
+import Login from './pages/Login'
+
+const routerConfig = [
+  {
+    path: '/login',
+    component: Login,
+  }
+]
+
+export default routerConfig
